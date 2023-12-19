32adb0e5a15b1e830a3da942276b6eb429691b0e
Daiki Hatada <hatadaiki@gmail.com>
2023-12-19 22:13:37 +0900

    Test Commit

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ async function main() {
   }
   const notion = new Notion(input.notionToken)
   const octokit = github.getOctokit(input.token)
+  console.log({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.payload.pull_request.number,
+  })
   const pullRequest = await octokit.rest.pulls.get({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
