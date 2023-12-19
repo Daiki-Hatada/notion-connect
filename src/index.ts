07d6ce4446069636efcb9b2c2c242d237f2aa4d5
Daiki Hatada <hatadaiki@gmail.com>
2023-12-19 21:58:17 +0900

    :bug: pull requestをfetchするコードのparamを修正

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,16 @@ import { notionUrlRegex, urlToPageId } from './utils/notion'
 import * as github from '@actions/github'
 
 async function main() {
+  if (!github.context.payload.pull_request?.number) {
+    throw new Error('This action is only available on pull_request event.')
+  }
   const notion = new Notion(input.notionToken)
   const octokit = github.getOctokit(input.token)
-  const pullRequest = await octokit.rest.pulls.get().then(({ data }) => data)
+  const pullRequest = await octokit.rest.pulls.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.payload.pull_request.number,
+  }).then(({ data }) => data)
   const comments = await octokit.rest.pulls
     .listReviewComments({
       owner: github.context.repo.owner,
